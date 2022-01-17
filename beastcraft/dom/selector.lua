bc1961ea53f08eb2e19a3cbc061ad9ce6fef2673
Jamie Nichols <Jamie337nichols@gmail.com>
2022-01-17 13:14:14 -0500

    Create BeastCraft Package Structure

---
+++ beastcraft/dom/selector.lua
@@ -0,0 +1,8 @@
+--[[ 
+[ Operations ]
+      getElementById(id:String):Element
+      getElementsByTagName(tagName:String):<Element>
+      getElementsByClassName(className:String)<Element>
+      querySelector(selector:String):Element
+      querySelectorAll(selector:String):<Element>
+]] 
