86bff1b428a6f256434766e0da378f4326163980
Jamie Nichols <Jamie337nichols@gmail.com>
2022-01-23 04:22:02 -0500

    Update startup

--- startup.lua
+++ startup.lua
@@ -1,6 +1,7 @@
-local drive = fs.getDir(shell.getRunningProgram())
-if drive == "disk" then
-     shell.run("pastebin", "run", "SNhCbPGA")
-else
-    shell.run("pastebin", "run", "kgTH67Ly")
-end
+-- local drive = fs.getDir(shell.getRunningProgram())
+-- if drive == "disk" then
+--      shell.run("pastebin", "run", "SNhCbPGA")
+-- else
+--     shell.run("pastebin", "run", "kgTH67Ly")
+-- end
+shell.run("main")
